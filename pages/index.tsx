110577eb9aaf60fe13e49a519f211fc12354b06e
Alvinotuya84 <alvinotuya8@gmail.com>
2022-11-27 01:36:37 +0300

    aslfkj

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home(     { pageInfo,
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20
      scrollbar-thumb-[#F7AB0A]/80">
       <Head>
-        <title>{pageInfo?.name}dfg</title>
+        <title>dfg{pageInfo?.name}</title>
         <meta name="description" content="Alvin Otuya Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
