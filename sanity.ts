02820d9e817e3aaec37394ae69bfcdf9daec0c81
Alvinotuya84 <alvinotuya8@gmail.com>
2022-11-26 22:42:33 +0300

    a;sdlkjf

--- sanity.ts
+++ sanity.ts
@@ -4,7 +4,7 @@ import createImageUrlBuilder from '@sanity/image-url'
 
 export const config={
     dataset:process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-    projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    projectId:"ixggk13a",
     apiVersion:"2021-03-25",
     useCdn:process.env.NODE_ENV ==="production"
 }
